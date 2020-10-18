91af0972f8ac772addd2beb6bfaa146084610adf
pasanyasaras <pasansmart@gmail.com>
2020-10-18 12:22:17 +0530

    crossing origin with frondend url

--- src/main/java/com/pasanyasara/EmployeeCRUDApplication/controller/EmployeeController.java
+++ src/main/java/com/pasanyasara/EmployeeCRUDApplication/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.pasanyasara.EmployeeCRUDApplication.controller;
 import com.pasanyasara.EmployeeCRUDApplication.model.Employee;
 import com.pasanyasara.EmployeeCRUDApplication.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/employees")
     public List<Employee> getAllEmployees()
     {
