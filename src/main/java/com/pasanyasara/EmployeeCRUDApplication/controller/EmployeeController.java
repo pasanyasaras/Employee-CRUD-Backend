5820c6d842f5d24c3229fa3f33f5b8fb360f954f
pasanyasaras <pasansmart@gmail.com>
2020-10-18 17:09:38 +0530

    creating REST API to Save Employee Objects

--- src/main/java/com/pasanyasara/EmployeeCRUDApplication/controller/EmployeeController.java
+++ src/main/java/com/pasanyasara/EmployeeCRUDApplication/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.pasanyasara.EmployeeCRUDApplication.controller;
 import com.pasanyasara.EmployeeCRUDApplication.model.Employee;
 import com.pasanyasara.EmployeeCRUDApplication.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,10 +14,19 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    //get all empolyees
     @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/employees")
     public List<Employee> getAllEmployees()
     {
+
         return employeeRepository.findAll();
+    }
+
+    //create employee REST api
+    @PostMapping("employees")
+    public Employee createEmployee(@RequestBody Employee employee)
+    {
+        return employeeRepository.save(employee);
     }
 }
