3d7fa1ce7aa3c33156fc09ec9f1036364ac095ab
pasanyasaras <pasansmart@gmail.com>
2020-10-20 23:10:08 +0530

    creating REST API to get single employee object

--- src/main/java/com/pasanyasara/EmployeeCRUDApplication/EmployeeCrudApplication.java
+++ src/main/java/com/pasanyasara/EmployeeCRUDApplication/EmployeeCrudApplication.java
@@ -2,12 +2,29 @@ package com.pasanyasara.EmployeeCRUDApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class EmployeeCrudApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmployeeCrudApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins(
+								"http://localhost:4200"
+						);
+			}
+		};
 	}
 
 }
--- src/main/java/com/pasanyasara/EmployeeCRUDApplication/controller/EmployeeController.java
+++ src/main/java/com/pasanyasara/EmployeeCRUDApplication/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.pasanyasara.EmployeeCRUDApplication.controller;
 
+import com.pasanyasara.EmployeeCRUDApplication.exception.ResourceNotFoundException;
 import com.pasanyasara.EmployeeCRUDApplication.model.Employee;
 import com.pasanyasara.EmployeeCRUDApplication.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,7 +17,7 @@ public class EmployeeController {
     private EmployeeRepository employeeRepository;
 
     //get all empolyees
-    @CrossOrigin(origins = "http://localhost:4200")
+    //@CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/employees")
     public List<Employee> getAllEmployees()
     {
@@ -27,6 +29,16 @@ public class EmployeeController {
     @PostMapping("employees")
     public Employee createEmployee(@RequestBody Employee employee)
     {
+
         return employeeRepository.save(employee);
+    }
+
+    //get employee by ID rest API
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id){
+        Employee employee = employeeRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not exit with id: "+id));
+
+        return ResponseEntity.ok(employee);
     }
 }
